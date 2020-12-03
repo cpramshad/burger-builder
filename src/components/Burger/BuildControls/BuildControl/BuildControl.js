887fde88d565eef8077d07b107aafd5d031575a8
cpramshad <cpramshad@gmail.com>
2020-12-03 19:25:54 +0400

    [feature] - Dynamically adding ingredients and updating price

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -4,8 +4,21 @@ import classes from './BuildControl.module.css';
 const BuildControl = props => (
   <div className={classes.BuildControl}>
     <div className={classes.Label}>{props.label}</div>
-    <button className={classes.Less} type="button">Less</button>
-    <button className={classes.More} type="button">More</button>
+    <button
+      className={classes.Less}
+      type="button"
+      onClick={props.removed}
+      disabled={props.disabled}
+    >
+      Less
+    </button>
+    <button
+      className={classes.More}
+      type="button"
+      onClick={props.added}
+    >
+      More
+    </button>
   </div>
 );
 
--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -11,9 +11,26 @@ const controls = [
 
 const BuildControls = props => (
   <div className={classes.BuildControls}>
+    <p>
+      Current Price:
+      <strong>{props.price.toFixed(2)}</strong>
+    </p>
     {controls.map(ctrl => (
-      <Buildcontrol key={ctrl.label} label={ctrl.label} />
+      <Buildcontrol
+        key={ctrl.label}
+        label={ctrl.label}
+        added={() => props.ingredientAdded(ctrl.type)}
+        removed={() => props.ingredientRemoved(ctrl.type)}
+        disabled={props.disabled[ctrl.type]}
+      />
     ))}
+    <button
+      type="button" 
+      className={classes.OrderButton}
+      disabled={!props.purchasable}
+    >
+      Order Now
+    </button>
   </div>
 );
 
