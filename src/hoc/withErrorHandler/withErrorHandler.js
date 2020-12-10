a447e6ea69cd1fe58a04b067246450503dec1939
cpramshad <cpramshad@gmail.com>
2020-12-10 19:43:05 +0400

    [bugfix] - Fixing linting issues

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,46 +1,44 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Modal from '../../components/UI/Modal/Modal';
 
-const withErrorHandler = (WrappedComponent, axios) => {
-  return class extends Component {
+const withErrorHandler = (WrappedComponent, axios) => class extends Component {
     state = {
-        error: null,
+      error: null,
     }
 
-      componentDidMount() {
-          axios.interceptors.request.use(req => {
-              this.setState({
-                  error: null,
-              })
-              return req;
-          })
-          axios.interceptors.response.use(res => res, error => {
-              this.setState({
-                  error: error,
-              })
-          })
-      }
+    componentDidMount() {
+      axios.interceptors.request.use(req => {
+        this.setState({
+          error: null,
+        });
+        return req;
+      });
+      axios.interceptors.response.use(res => res, error => {
+        this.setState({
+          error,
+        });
+      });
+    }
 
       errorConfirmedHandler = () => {
-          this.setState({
-              error: null,
-          })
+        this.setState({
+          error: null,
+        });
       }
 
       render() {
-          return (
-            <>
-              <Modal
-                show={this.state.error}
-                modalClosed={this.errorConfirmedHandler}
-              >
-                {this.state.error? this.state.error.message : null}
-              </Modal>
-              <WrappedComponent {...this.props} />
-            </>
-          )
+        return (
+          <>
+            <Modal
+              show={this.state.error}
+              modalClosed={this.errorConfirmedHandler}
+            >
+              {this.state.error ? this.state.error.message : null}
+            </Modal>
+            <WrappedComponent {...this.props} />
+          </>
+        );
       }
-  }
-}
+};
 
 export default withErrorHandler;
