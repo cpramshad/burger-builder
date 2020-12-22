e1b5cbc0485b0986384cac08e39355356a7c88ad
cpramshad <cpramshad@gmail.com>
2020-12-22 20:55:04 +0400

    [bugfix] Linting issue fix

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -5,36 +5,34 @@ import { connect } from 'react-redux';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
 
-
 class Checkout extends Component {
-
     checkoutCancelledHandler = () => {
-        this.props.history.goBack();
+      this.props.history.goBack();
     }
 
     checkoutContinuedHandler = () => {
-        this.props.history.replace( '/checkout/contact-data' );
+      this.props.history.replace('/checkout/contact-data');
     }
 
-    render () {
-        return (
-            <div>
-                <CheckoutSummary
-                    ingredients={this.props.ings}
-                    checkoutCancelled={this.checkoutCancelledHandler}
-                    checkoutContinued={this.checkoutContinuedHandler} />
-                <Route 
-                    path={this.props.match.path + '/contact-data'} 
-                    component={ContactData} />
-            </div>
-        );
+    render() {
+      return (
+        <div>
+          <CheckoutSummary
+            ingredients={this.props.ings}
+            checkoutCancelled={this.checkoutCancelledHandler}
+            checkoutContinued={this.checkoutContinuedHandler}
+          />
+          <Route
+            path={`${this.props.match.path}/contact-data`}
+            component={ContactData}
+          />
+        </div>
+      );
     }
 }
 
-const mapStateToProps = state => {
-    return {
-        ings: state.ingredients
-    };
-}
+const mapStateToProps = state => ({
+  ings: state.ingredients,
+});
 
 export default connect(mapStateToProps)(Checkout);
