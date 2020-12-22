800fb6ebd538cb5524a66121f3070685c2092757
cpramshad <cpramshad@gmail.com>
2020-12-22 20:52:55 +0400

    [feat] Adding redux to containers

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -49,20 +49,7 @@ class BurgerBuilder extends Component {
         this.setState( { purchasing: false } );
     }
 
-    purchaseContinueHandler = () => {
-        // alert('You continue!');
-        
-        const queryParams = [];
-        for (let i in this.state.ingredients) {
-            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
-        }
-        queryParams.push('price=' + this.state.totalPrice);
-        const queryString = queryParams.join('&');
-        this.props.history.push({
-            pathname: '/checkout',
-            search: '?' + queryString
-        });
-    }
+    purchaseContinueHandler = () => { this.props.history.push('/checkout'); }
 
     render () {
         const disabledInfo = {
