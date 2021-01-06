b8726f493560cc9cb57541bf2f2193a97f9e61d5
cpramshad <cpramshad@gmail.com>
2021-01-06 22:24:47 +0400

    [feature] - 340 - Displaying only user data

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -46,9 +46,10 @@ export const fetchOrdersStart = () => ({
   type: actionTypes.FETCH_ORDERS_START,
 });
 
-export const fetchOrders = (token) => dispatch => {
+export const fetchOrders = (token, userId) => dispatch => {
   dispatch(fetchOrdersStart());
-  axios.get('/orders.json?auth=' + token)
+  const queryParams = '?auth=' + token + '&orderBy="userId"&equalTo="' + userId + '"';
+  axios.get('/orders.json' + queryParams)
     .then(res => {
       const fetchedOrders = [];
       for (const key in res.data) {
